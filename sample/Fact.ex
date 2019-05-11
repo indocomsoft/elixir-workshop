6a6214be2a78a680ed1e207dc4c2492abb4a085b
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-05-11 13:42:57 +0800

    Fix some more typos

--- sample/Fact.ex
+++ sample/Fact.ex
@@ -1,5 +1,5 @@
 defmodule Fact do
-  def fac(n, acc \\ 1) when is_number(n) and is_number(acc) do
+  def fac(n, acc \\ 1) when is_integer(n) and is_integer(acc) and acc > 0 do
     case n do
       0 -> acc
       n when n > 0 -> fac(n - 1, acc * n)
